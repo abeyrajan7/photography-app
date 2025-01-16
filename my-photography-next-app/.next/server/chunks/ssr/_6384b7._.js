0ba9bf5b50d0b56b257f4ae244737feda3e3a7b4
Abey K Rajan <abeyrajan7@gmail.com>
2025-01-16 02:23:47 -0500

    default page load

--- my-photography-next-app/.next/server/chunks/ssr/_6384b7._.js
+++ my-photography-next-app/.next/server/chunks/ssr/_6384b7._.js
@@ -13,7 +13,6 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist
 ;
 function Home() {
     (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$components$2f$navigation$2e$react$2d$server$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["redirect"])("/about");
-    return null;
 }
 }}),
 "[project]/src/app/page.tsx [app-rsc] (ecmascript, Next.js server component)": ((__turbopack_context__) => {
