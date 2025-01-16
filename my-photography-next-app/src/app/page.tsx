2f4d20c51ef77e18acd8cfbfe440a5ac20f15c46
Abey K Rajan <abeyrajan7@gmail.com>
2025-01-16 10:40:03 -0500

    build issue fix-1

--- my-photography-next-app/src/app/page.tsx
+++ my-photography-next-app/src/app/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/about");
-  }, [router]);
-
-  return null; // Avoid rendering during redirection
+  return <h1>Welcome to the Photography App</h1>;
 }
