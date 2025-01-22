79956ff1e9a95513029e9e0761babdd01636006f
Abey K Rajan <abeyrajan7@gmail.com>
2025-01-22 00:31:15 -0500

    backend deployment

--- photography-backend/server.js
+++ photography-backend/server.js
@@ -5,9 +5,10 @@ const AWS = require("aws-sdk");
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION || "ap-south-1", // Use fallback if .env is not loaded
 });
 
+console.log("AWS_REGION:", process.env.AWS_REGION);
 const express = require("express");
 const app = express();
 const port = 3001;
