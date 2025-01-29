102d315e77e2d2a3c2e3a15a9095ce24acb25b92
Abey K Rajan <abeyrajan7@gmail.com>
2025-01-29 02:07:54 -0500

    responsive

--- my-photography-next-app/src/Components/Header.tsx
+++ my-photography-next-app/src/Components/Header.tsx
@@ -2,32 +2,40 @@
 
 import React from "react";
 import "./Header.css";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 
 export default function Header() {
   const router = useRouter();
+  const [activeTab, setActiveTab] = useState("/about");
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleNavigation = (path: string) => {
+    setActiveTab(path);
     router.push(path);
+    setMenuOpen(false);
   };
 
   return (
     <div className="header-bar">
       <h1 className="site-title">Frame Finder</h1>
-      <ul className="nav-items">
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <FaBars size={24} />
+      </div>
+      <ul className={`nav-items ${menuOpen ? "open" : ""}`}>
         <li
           onClick={() => handleNavigation("/about")}
-          style={{ cursor: "pointer", color: "blanchedalmond" }}
+          className={activeTab === "/about" ? "active-tab" : ""}
         >
           About
         </li>
         <li
           onClick={() => handleNavigation("/gallery")}
-          style={{ cursor: "pointer", color: "blanchedalmond" }}
+          className={activeTab === "/gallery" ? "active-tab" : ""}
         >
-          Photography Galary
+          Photography
         </li>
-        {/* <li style={{ cursor: "pointer", color: "blanchedalmond" }}>Contact</li> */}
       </ul>
     </div>
   );
