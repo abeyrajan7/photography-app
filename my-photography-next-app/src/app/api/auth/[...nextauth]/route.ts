3c0fd8c547f441ef56fcd47ea397f9b65263f092
Abey K Rajan <abeyrajan7@gmail.com>
2025-02-12 14:41:30 -0500

    auth issue

--- my-photography-next-app/src/app/api/auth/[...nextauth]/route.ts
+++ my-photography-next-app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,32 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const authOptions = {
+const authOptions : NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          redirect_uri: "https://photography-app-5osi.vercel.app/api/auth/callback/google",
+        },
+      },
     }),
   ],
+  pages: {
+    signIn: "/auth/signin", // Custom sign-in page (optional)
+  },
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
 
-export const GET = handler;
-export const POST = handler;
+export { handler as GET, handler as POST };
